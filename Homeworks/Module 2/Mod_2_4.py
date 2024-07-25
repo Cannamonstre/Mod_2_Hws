7bb6db2f41234811089707aa187445c0fcfc743f
Dmitry <prrtaf@yandex.ru>
2024-07-25 20:03:49 +0300

    Update Mod_2_4.py
    Increasing readability

--- Homeworks/Module 2/Mod_2_4.py
+++ Homeworks/Module 2/Mod_2_4.py
@@ -2,6 +2,7 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 neutral_shit_breaking_my_code = []
+
 for i in numbers:
     is_prime = True
     for j in range(2, i):
@@ -14,6 +15,7 @@ for i in numbers:
         primes.append(i)
     else:
         not_primes.append(i)
+
 print('Primes: ', primes)
 print('Composites: ', not_primes)
 print('Neutral: ', neutral_shit_breaking_my_code)
